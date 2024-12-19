5a117ff01923313442432877dbb43f1c425e1004
Ovidiu Turcoane <ovidiu.turcoane@gmail.com>
2024-12-19 16:58:01 +0200

    Story : Explore TDD on JS. Part two - suite.

--- app/static/js/test/test_dom.js
+++ app/static/js/test/test_dom.js
@@ -1,23 +1,20 @@
 import {buildAppTable} from "../utils.js";
 import {given_Desc_thenRunMethod} from "./test_utils.js";
 
-(function () {
+export const test_dom = () => {
     'use strict';
-
-    window.onload = () => {
-        given_Desc_thenRunMethod('should find table', function () {
-            const elements = document.querySelectorAll('[data-test-id=test-div-table]');
-            const mock_data = {
-                "id": "1",
-                "name": "test",
-                "ingredients": ["water", "soda"],
-                "instructions": "drink"
-            };
-            let result = elements[0] != null;
-            buildAppTable(mock_data, "../../static/icons/");
-            const checkBtn = document.getElementById('Add-1');
-            result = result && checkBtn != null;
-            document.getElementById("id-test-table-items").innerText = result.toString();
-        });
-    }
-})();
+    given_Desc_thenRunMethod('should find DOM table and row elements', function () {
+        const elements = document.querySelectorAll('[data-test-id=test-div-table]');
+        const mock_data = {
+            "id": "1",
+            "name": "test",
+            "ingredients": ["water", "soda"],
+            "instructions": "drink"
+        };
+        let result = elements[0] != null;
+        buildAppTable(mock_data, "../../static/icons/");
+        const checkBtn = document.getElementById('Add-1');
+        result = result && checkBtn != null;
+        document.getElementById("id-test-table-items").innerText = result.toString();
+    });
+}
